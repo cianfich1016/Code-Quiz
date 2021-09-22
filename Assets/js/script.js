e05259b78eefd04edab4aa195613d9b34e79235c
Rachel Cianfichi <cianfich@gmail.com>
2021-09-21 22:41:38 -0400

    Added all elements dynamically in js file. Adjusted question array to make each answer an object instead of an array . Created displayQuestions() function aChanged answers in question array to their own objects. Solidified (for now) displayQuestions() function. Stylized question and the answers.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,84 +2,108 @@
 var timerElement = document.querySelector(".timerText");
 var startButton = document.querySelector(".startButton");
 var firstPage = document.getElementById("firstPage");
-var quiz = document.getElementById("quiz");
 
+var quizEl = document.querySelector("#quiz");
+var questionsEl = document.createElement("h2");
+var listEl = document.createElement("ol");
+var li1 = document.createElement("button");
+var li2 = document.createElement("button");
+var li3 = document.createElement("button");
+var li4 = document.createElement("button");
 
+quizEl.appendChild(questionsEl);
+quizEl.appendChild(listEl);
+listEl.appendChild(li1);
+listEl.appendChild(li2);
+listEl.appendChild(li3);
+listEl.appendChild(li4);
 
+questionsEl.setAttribute("style", "font-size: 25px")
+
+li1.setAttribute("style", "margin: 3px; padding: 7px; background-color: purple; color: white; font-size: 15px; display: flex; flex-direction: column")
+li2.setAttribute("style", "margin: 3px; padding: 7px; background-color: purple; color: white; font-size: 15px; display: flex; flex-direction: column")
+li3.setAttribute("style", "margin: 3px; padding: 7px; background-color: purple; color: white; font-size: 15px; display: flex; flex-direction: column")
+li4.setAttribute("style", "margin: 3px; padding: 7px; background-color: purple; color: white; font-size: 15px; display: flex; flex-direction: column")
+
+var currentQuestion = 0;
 var secondsRemaining = 75;
 
 var quizQuestions = [
     {
         question: "Who created JavaScript?",
-        answers: {
-            1: "Donald D. Chamberlin",
-            2: "Mark Zuckerberg",
-            3: "Tim Berners-Lee",
-            4: "Brendan Eich",
-        } , 
-        correctChoice: "4", 
+        answerA: " a. Donald D. Chamberlin",
+        answerB: " b. Mark Zuckerberg",
+        answerC: "c. Tim Berners-Lee",
+        answerD:  " d. Brendan Eich", 
+        correctChoice: "d", 
     },
     {
         question: "What functionality does JavaScript provide to websites?",
-        answers: {
-            1: "Structure of the page",
-            2: "Styling of the page",
-            3: "Interactivity of the page",
-            4: "None of the above",
-        } , 
-        correctChoice: "3", 
+        answerA: "a.Structure of the page",
+        answerB: "b. Styling of the page", 
+        answerC: "c. Interactivity of the page", 
+        answerD: "d. None of the above",
+        correctChoice: "c",
     },
     {
         question: "What marks the end of a JavaScript statement?",
-        answers: {
-            1: "semicolon",
-            2: "comma",
-            3: "parenthesis",
-            4: "colon",
-        },
-        correctChoice: "1",
+        answerA: "a. semicolon", 
+        answerB: "b. comma", 
+        answerC: "c. parenthesis", 
+        answerD: "d. colon",
+        correctChoice: "a",
     },
     {
         question: "All are common data types used EXCEPT which of the following:",
-        answers: {
-            1: "boolean",
-            2: "prompt",
-            3: "number",
-            4: "symbol",
-        } , 
-        correctChoice: "2", 
+        answerA: "a. boolean", 
+        answerB: "b. prompt", 
+        answerC: "c. number", 
+        answerD: "d. symbol", 
+        correctChoice: "b", 
     },
     {
         question: "What function can be used to display any message to the user?",
-        answers: {
-            1: "console.view())",
-            2: "console.filter()",
-            3: "console.find()",
-            4: "console.log()",
-        } , 
-        correctChoice: "4", 
+        answerA: "a. console.view()", 
+        answerB: "b. console.filter()", 
+        answerC: "c. console.find()", 
+        answerD: "d. console.log()",
+        correctChoice: "d", 
     },
    
 ];
 
+quizEl.style.display = "none";
+
 function startQuiz(){
     firstPage.style.display = "none";
+    quizEl.style.display = "flex";
     secondsRemaining = 75;
     startClock();
-    startQuestions();
-}
+    displayQuestions();
+};
 
-function startQuestions(){
+function displayQuestions(){
     
-        
-}
+    for(var i=0; i < quizQuestions.length; i++){
+        questionsEl.textContent = quizQuestions[currentQuestion].question;
+        li1.textContent = quizQuestions[currentQuestion].answerA;
+        li2.textContent = quizQuestions[currentQuestion].answerB;
+        li3.textContent = quizQuestions[currentQuestion].answerC;
+        li4.textContent = quizQuestions[currentQuestion].answerD;
+        i++;
+    }  
+   
+    //var correctChoice = quizQuestions[currentQuestion].correctChoice;     
+};
+
+
+
 
 function startClock(){
     var timer = setInterval(function() {
         secondsRemaining--;
         timerElement.textContent = "Time: " + secondsRemaining + " seconds";
         if (secondsRemaining === 0){
-            console.log ("lose")
             clearInterval(timer);
         }
 
@@ -89,5 +113,12 @@ function startClock(){
 
 
 
+li1.addEventListener("click", displayQuestions);
+li2.addEventListener("click", displayQuestions);
+li3.addEventListener("click", displayQuestions);
+li4.addEventListener("click", displayQuestions); 
 
+function buttonclick(){
+    console.log("yay")
+}
 startButton.addEventListener("click", startQuiz);
